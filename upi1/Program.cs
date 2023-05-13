acfbe8a2fca2858bb3354df963a0f78b97e4069f
Crause <elcarousel@mail.ru>
2023-05-13 10:48:23 +0300

    'start'

---
+++ upi1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace upi1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер задания [1 или 2]:");
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    work1();
+                    break;
+                case "2":
+                    work2();
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("Работа завершена.");
+            Console.ReadKey();
+
+        }
+
+        static void work1()
+        {
+
+        }
+
+        static void work2()
+        {
+
+        }
+    }
+}
